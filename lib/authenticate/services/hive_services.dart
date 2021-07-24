9409c49a1f6b86af86b745c965be978c8108dcc1
Sarvesh Bhatnagar <sarveshbhatnagar08@gmail.com>
2021-07-24 17:17:46 +0530

    added hive services

---
+++ lib/authenticate/services/hive_services.dart
@@ -0,0 +1,38 @@
+import 'package:hive/hive.dart';
+
+class HiveAuthServices {
+  /// The authentication service.
+  var box = Hive.box("authbox");
+
+  /// Puts login information into the database.
+  putlogin(String email, String password) {
+    box.put("email", email);
+    box.put("password", password);
+  }
+
+  /// Gets the email from the database.
+  getEmail() {
+    return box.get("email");
+  }
+
+  /// Gets the password from the database.
+  getPassword() {
+    return box.get("password");
+  }
+
+  /// Sets the information stored flag.
+  setInformationStored(bool stored) {
+    box.put("informationStored", stored);
+  }
+
+  /// Gets the information stored flag.
+  getInformationStored() {
+    return box.get("informationStored");
+  }
+
+  /// Clears the login information.
+  clearLogin() {
+    box.clear();
+    setInformationStored(false);
+  }
+}
