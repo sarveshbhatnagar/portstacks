50cca24a5141baefd6f52053fd0da8100a78178b
Sarvesh Bhatnagar <sarveshbhatnagar08@gmail.com>
2021-07-24 17:34:06 +0530

    converted basepage to stateful widget
    Did this to support initState so I can login if login information is available on init.

--- lib/authenticate/screens/basePage.dart
+++ lib/authenticate/screens/basePage.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
-class BasePage extends StatelessWidget {
+class BasePage extends StatefulWidget {
+  @override
+  _BasePageState createState() => _BasePageState();
+}
+
+class _BasePageState extends State<BasePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
